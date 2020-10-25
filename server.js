94ddeb4cdc38498b87949f11597c64ec20c6ff90
joudi awameh <awamehj@gmail.com>
2020-10-25 20:32:00 +0300

    weather

--- server.js
+++ server.js
@@ -3,21 +3,29 @@ let app = express();
 let cors = require('cors');
 
 app.use(cors());
-require ('dotenv').config();
+require('dotenv').config();
 
-app.get('/location' , handelLocation);
-function handelLocation (req,res) {
-    let city = req.query.city;
-    let jasondata = require('./data/location.json');
-    let jasonobject = jasondata[0];
-    let newlocation = new Location(city, jasonobject.display_name ,jasonobject.lat ,jasonobject.lon);
-    res.status(200).json(newlocation);
+app.get('/location', handelLocation);
+function handelLocation(req, res) {
+
+    try {
+        let city = req.query.city;
+        let jasondata = require('./data/location.json');
+        let jasonobject = jasondata[0];
+        let newlocation = new Location(city, jasonobject.display_name, jasonobject.lat, jasonobject.lon);
+        res.status(200).json(newlocation);
+    } catch (error) {
+        res.status(500).send('Sorry, something went wrong');
+    }
+
+
 }
-function Location (search_query,formatted_query,latitude,longitude){
-    this.search_query =search_query;
-    this.formatted_query= formatted_query;
-    this.latitude= latitude;
-    this.longitude =longitude;
+
+function Location(search_query, formatted_query, latitude, longitude) {
+    this.search_query = search_query;
+    this.formatted_query = formatted_query;
+    this.latitude = latitude;
+    this.longitude = longitude;
 }
 // "search_query": "seattle",
 // "formatted_query": "Seattle, WA, USA",
@@ -25,8 +33,52 @@ function Location (search_query,formatted_query,latitude,longitude){
 // "longitude": "-122.332071"
 // }
 
-const  PORT = process.env.PORT;
-app.listen(PORT, ()=>{
+
+
+app.get('/weather', handelweather);
+function handelweather(req, res) {
+    try {
+
+        let jasondata = require('./data/weather.json');
+        let jasonobject = jasondata.data;
+        let arr = [];
+
+        for (let i = 0; i < jasonobject.length; i++) {
+
+            let newWeather = new Weather(jasonobject[i].weather.description, jasonobject[i].datetime);
+            arr.push(newWeather);
+
+        }
+
+
+        res.status(200).send(arr);
+        
+
+
+
+
+    } catch (error) {
+        res.status(500).send('Sorry, something went wrong');
+    }
+}
+function Weather(forecast, time) {
+    this.forecast = forecast;
+    this.time = new Date(time).toDateString();
+}
+
+// [
+//     {
+//       "forecast": "Partly cloudy until afternoon.",
+//       "time": "Mon Jan 01 2001"
+//     },
+//     {
+//       "forecast": "Mostly cloudy in the morning.",
+//       "time": "Tue Jan 02 2001"
+//     },
+//     ...
+//   ]
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
     console.log('this is the listen ');
 })
 
