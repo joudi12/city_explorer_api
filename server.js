06dbd72df6fd6b4a3203e44d690b4b5a1faa4525
joudi awameh <awamehj@gmail.com>
2020-10-26 17:23:53 +0300

    did lab07

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 let cors = require('cors');
+let superagent = require('superagent');
 
 app.use(cors());
 require('dotenv').config();
@@ -8,62 +9,76 @@ require('dotenv').config();
 app.get('/location', handelLocation);
 function handelLocation(req, res) {
 
-    try {
-        let city = req.query.city;
-        let jasondata = require('./data/location.json');
-        let jasonobject = jasondata[0];
-        let newlocation = new Location(city, jasonobject.display_name, jasonobject.lat, jasonobject.lon);
-        res.status(200).json(newlocation);
-    } catch (error) {
-        res.status(500).send('Sorry, something went wrong');
-    }
+
+  let city = req.query.city;
+  let key = process.env.GEOCODE_API_KEY;
+  superagent.get(`https://eu1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`)
+    .then((data) => {
+      let jasonobject = data.body[0];
+
+      let newlocation = new Location(city, jasonobject.display_name, jasonobject.lat, jasonobject.lon);
+      res.status(200).json(newlocation);
+    }).catch(() => {
+      res.status(500).send('Sorry, something went wrong');
+    });
+  // let jasondata = require('./data/location.json');
+
+
+  //   } catch (error) {
+  //     res.status(500).send('Sorry, something went wrong');
+  //   }
 
 
 }
 
 function Location(search_query, formatted_query, latitude, longitude) {
-    this.search_query = search_query;
-    this.formatted_query = formatted_query;
-    this.latitude = latitude;
-    this.longitude = longitude;
+  this.search_query = search_query;
+  this.formatted_query = formatted_query;
+  this.latitude = latitude;
+  this.longitude = longitude;
 }
-// "search_query": "seattle",
-// "formatted_query": "Seattle, WA, USA",
-// "latitude": "47.606210",
-// "longitude": "-122.332071"
-// }
 
 
 
 app.get('/weather', handelweather);
 function handelweather(req, res) {
-    try {
-
-        let jasondata = require('./data/weather.json');
-        let jasonobject = jasondata.data;
-        let arr = [];
-
-        for (let i = 0; i < jasonobject.length; i++) {
-
-            let newWeather = new Weather(jasonobject[i].weather.description, jasonobject[i].datetime);
-            arr.push(newWeather);
-
-        }
 
 
-        res.status(200).send(arr);
-        
+  // let jasondata = require('./data/weather.json');
+  let city = req.query.search_query;
+  let key = process.env.WEATHER_API_KEY;
+  //   let lat =req.query.jasonobject.lat;
+  //   let lon =req.query.jasonobject.lon;
+
+  superagent.get(`https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${key}
+    `).then((val) => {
+    let jasonobject = val.body.data;
+
+    let arr = jasonobject.map((value) => {
+      return new Weather(value.weather.description, value.datetime);
+
+    });
+    console.log(arr);
+    res.status(200).send(arr);
+  }).catch(() => {
+    res.status(500).send('Sorry, something went wrong');
+  });
 
 
 
+  // let arr = [];
 
-    } catch (error) {
-        res.status(500).send('Sorry, something went wrong');
-    }
+  // for (let i = 0; i < jasonobject.length; i++) {
+
+  //   let newWeather = new Weather(jasonobject[i].weather.description, jasonobject[i].datetime);
+  //   arr.push(newWeather);
+
+  // }
+
 }
 function Weather(forecast, time) {
-    this.forecast = forecast;
-    this.time = new Date(time).toDateString();
+  this.forecast = forecast;
+  this.time = new Date(time).toDateString();
 }
 
 // [
@@ -77,8 +92,57 @@ function Weather(forecast, time) {
 //     },
 //     ...
 //   ]
+app.get('/trails', handeltrails);
+function handeltrails(req, res) {
+
+
+
+
+  let key = process.env.TRAIL_API_KEY;
+  //   let lat =req.query.jasonobject.lat;
+  //   let lon =req.query.jasonobject.lon;
+
+  superagent.get(`https://www.hikingproject.com/data/get-trails?lat=${req.query.latitude}&lon=${req.query.longitude}&maxDistance=200&key=${key}
+ 
+    `).then((val) => {
+    let jasonobject = val.body.trails;
+    console.log(jasonobject);
+    let arr = jasonobject.map((value) => {
+      return new Trail(value);
+
+    });
+    console.log(arr);
+    res.status(200).send(arr);
+  }).catch(() => {
+    res.status(500).send('Sorry, something went wrong');
+  });
+}
+function Trail(traildata) {
+  this.name = traildata.name;
+  this.location = traildata.location;
+  this.length = traildata.length;
+  this.stars = traildata.stars;
+  this.star_votes = traildata.starVotes;
+  this.summary = traildata.summary;
+  this.trails_url = traildata.url;
+  this.conditions = traildata.conditionStatus;
+  this.condition_date = traildata.conditionDate.toString().slice(0,10);
+  this.condition_time = traildata.conditionDate.toString().slice(11,20);
+}
+//   {
+//     "name": "Rattlesnake Ledge",
+//     "location": "Riverbend, Washington",
+//     "length": "4.3",
+//     "stars": "4.4",
+//     "star_votes": "84",
+//     "summary": "An extremely popular out-and-back hike to the viewpoint on Rattlesnake Ledge.",
+//     "trail_url": "https://www.hikingproject.com/trail/7021679/rattlesnake-ledge",
+//     "conditions": "Dry: The trail is clearly marked and well maintained.",
+//     "condition_date": "2018-07-21",
+//     "condition_time": "0:00:00 "
+//   },
+//   {
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log('this is the listen ');
-})
-
+  console.log('this is the listen ');
+});
